b2b56555f735d8437eb8be228c61411c95f3577b
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-02-25 20:23:00 -0500

    Add a tool to convert an height map to a json like string.

---
+++ png_converter/png_converter/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.Drawing
+
+let convertLine (y:int) (bitmap:System.Drawing.Bitmap) =
+    [ for x in 0 .. bitmap.Width - 1 -> "\"" + bitmap.GetPixel(x, y).R.ToString() + "\"" ] |> String.concat ", "
+
+let convert (path:string) : string = 
+    let bitmap = new System.Drawing.Bitmap(path)
+    let results = [ for y in 0 .. bitmap.Height - 1 -> convertLine y bitmap ] |> String.concat ", "
+   
+    "{\"X\": \"" + bitmap.Width.ToString() + 
+    "\", \"Y\": \"" + bitmap.Width.ToString() + 
+    "\", \"values\":[" + results + "]}"
+
+[<EntryPoint>]
+let main argv = 
+    let arglist = argv |> List.ofSeq
+
+    let returnValue = 
+        match arglist with
+            | path :: _ -> convert path
+            | _ -> "Wrong number of arguments supplied"
+
+    printfn "%s" returnValue
+    
+    match returnValue with
+        | "Wrong number of arguments supplied" -> 1
+        | _ -> 0

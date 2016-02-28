a635c9061c606b82554455d2949100db77dfd8c9
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-02-28 10:56:29 -0500

    Update test runnner.
    Basic functionality now working.

--- terrain_gen/tests.fs
+++ terrain_gen/tests.fs
@@ -5,18 +5,30 @@ open MidpointDisplacement
 
 // assert functions
 let assertAreEqual expected actual =
+    printf "... "
     if expected <> actual then printfn "%s" ("Test failed, expected " + expected.ToString() + ", actual " + actual.ToString())  
-    else ()
+    else printfn "Test passed"
 
 // tests
-let ``test newHeightMap will return a 0 initialized height map`` = 
+let testNewHeightMapReturnZeroInitializedHm () = 
     let hm = newHeightMap 5
     let result = hm.Map |> Array.sum
-    assertAreEqual 1.0 result
+    assertAreEqual 0.0 result
+
+// tests included in run
+let testsToRun = 
+    [
+        ("test newHeightMap will return a 0 initialized height map",
+        testNewHeightMapReturnZeroInitializedHm)
+    ]
 
 // test runner
+let runSingleTest test = 
+    let testName, testFunction = test
+    printf "%s" testName    
+    testFunction()
+
 let runTests =
-    let testsToRun = [``test newHeightMap will return a 0 initialized height map``]    
-    testsToRun |> List.map (fun f -> f)
+    testsToRun |> List.map (fun t -> runSingleTest t)
     printfn "%s" "Ran all tests."
     

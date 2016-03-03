2d41689a8b2e292b12792f361008263294717141
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-03-02 22:27:54 -0500

    Add function to create png from height map.
    Currently only creates a black png. Needs to read values from height map.

--- terrain_gen/terrain_gen.fs
+++ terrain_gen/terrain_gen.fs
@@ -1,11 +1,25 @@
 module TerrainGen
 
+open System.Drawing
+
 open HeightMap  
 open MidpointDisplacement
 open TestFramework
 open Tests
 
+let heightMapToPng (heightMap:HeightMap) (filename:string) =
+    let png = new Bitmap(heightMap.Size, heightMap.Size)
+    for x in [0..heightMap.Size-1] do
+        for y in [0..heightMap.Size-1] do
+            // will need to convert float 1.0 to 255 with conversion function
+            // let greyScale = heightMap.Get x y 
+            png.SetPixel(x, y, Color.FromArgb(255, 0, 0, 0))
+    
+    png.Save(filename, Imaging.ImageFormat.Png) |> ignore
+
 [<EntryPoint>]
 let main argv =
-    consoleTestRunner testsToRun   
+    consoleTestRunner testsToRun
+    let map = newHeightMap 10
+    heightMapToPng map "out.png"  
     0 

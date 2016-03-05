c6653f3ac09b3dba6e217f0813159ae1a84b2a4f
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-03-05 09:19:45 -0500

    Refactor if else expression into match expression.

--- terrain_gen/height_map.fs
+++ terrain_gen/height_map.fs
@@ -10,17 +10,16 @@ type HeightMap = {Size:int; Map:float array} with
 // returns a square matrix of size 2^n + 1
 let newHeightMap n : HeightMap =
     let size = ( pown 2 n ) + 1
-    {Size = size; Map = Array.zeroCreate (size * size)}
+    {Size = size; Map = Array.zeroCreate (size * size)}  
 
 // normalize a height map to constrain all of it's values between 0.0 and 1.0
 let normalize matrix =
+    // todo: check to replace with match
     matrix |> Array.map (fun a ->
-                            if a < 0.0 then
-                                0.0
-                            else if a > 1.0 then
-                                1.0
-                            else
-                                a)
+                            match a with
+                            | a when a < 0.0 -> 0.0
+                            | a when a > 1.0 -> 1.0
+                            | _ -> a)
 
 // converts a float point ranging from 0.0 to 1.0 to a rgb value
 // 0.0 represents black and 1.0 white. The conversion is in greyscale 

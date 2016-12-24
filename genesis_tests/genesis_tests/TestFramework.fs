64d53e7f977419f9e2933a7d81539069587cfd70
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-12-23 20:35:30 -0500

    Refactor asserts to use match instead of if    conditional.

--- genesis_tests/genesis_tests/TestFramework.fs
+++ genesis_tests/genesis_tests/TestFramework.fs
@@ -13,18 +13,15 @@ let assertIsTrue value =
     | true -> passed
     | false -> "Test failed, expected true but was false"
 
-// todo: replace with matches
 let assertAreEqual expected actual =
-    if expected <> actual then 
-        sprintf "Test failed, expected %A, actual %A" expected actual  
-    else 
-        passed
+    match expected <> actual with
+    | true -> sprintf "Test failed, expected %A, actual %A" expected actual  
+    | false -> passed
         
 let assertIsGreaterThan target actual =
-     if target >= actual then
-        sprintf "Test failed, expected %A to be greater than %A" target actual
-     else
-        passed 
+     match target >= actual with
+     | true -> sprintf "Test failed, expected %A to be greater than %A" target actual
+     | false -> passed 
         
 // test runner
 let runSingleTest (testName, testFunction) = 

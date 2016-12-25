d381bb9278b31c185b06fe1143206eee804d6f20
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-12-25 11:28:19 -0500

    Fix spelling of occurence to occurrence

--- genesis/genesis/name_gen/NameGenerator.fs
+++ genesis/genesis/name_gen/NameGenerator.fs
@@ -7,27 +7,27 @@ open System.Collections.Generic
 let readInputFile (filePath:string) = 
     System.IO.File.ReadAllText(filePath)
 
-// Parses a string and count the total number of occurences of substrings of size length
-let rec countOccurences (input:string) (occurenceTable:Dictionary<string, int>) length = 
+// Parses a string and count the total number of occurrences of substrings of size length
+let rec countOccurrences (input:string) (occurrenceTable:Dictionary<string, int>) length = 
     let adjLen = length - 1
     
     match input |> Seq.toList with
     | head :: tail when tail.Length >= adjLen -> 
         let other = Seq.take adjLen tail |> Seq.toList
-        let occurence = (head :: other |> Array.ofList |> String)
+        let occurrence = (head :: other |> Array.ofList |> String)
 
-        // add current occurence to the occurence table
-        match occurenceTable.ContainsKey (occurence) with
-        | true -> occurenceTable.[occurence] <- occurenceTable.[occurence] + 1
-        | false -> occurenceTable.Add(occurence, 1)
+        // add current occurrence to the occurrence table
+        match occurrenceTable.ContainsKey (occurrence) with
+        | true -> occurrenceTable.[occurrence] <- occurrenceTable.[occurrence] + 1
+        | false -> occurrenceTable.Add(occurrence, 1)
 
         // call the function recursively with the rest of the string
-        countOccurences (tail |> Array.ofList |> String) occurenceTable length
-    | _ -> occurenceTable
+        countOccurrences (tail |> Array.ofList |> String) occurrenceTable length
+    | _ -> occurrenceTable
 
 // Given an input file create a probability table for the different letters in the file
 let buildProbabilityTables (filePath:string) length =
     let input = readInputFile filePath
     let initialDictionary = new Dictionary<string, int>()
 
-    countOccurences input initialDictionary length
+    countOccurrences input initialDictionary length
--- genesis_tests/genesis_tests/NameGenTests.fs
+++ genesis_tests/genesis_tests/NameGenTests.fs
@@ -10,75 +10,75 @@ open NameGenerator
 // tests included in run
 let nameGenTests = 
     [
-        "countOcccurence will add a new entry for a new string occurence and init to 1",
+        "countOcccurrence will add a new entry for a new string occurrence and init to 1",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 1
+            let result = countOccurrences "abcdefg" dict 1
             assertAreEqual 1 result.["a"];
 
-        "countOcccurence will return 0 for a string that is not in the input",
+        "countOcccurrence will return 0 for a string that is not in the input",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 1
+            let result = countOccurrences "abcdefg" dict 1
             assertIsFalse (result.ContainsKey "z");    
 
-        "countOcccurence will return 2 occurrences if a string occures twice",
+        "countOcccurrence will return 2 occurrences if a string occures twice",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "aabcdefg" dict 1
+            let result = countOccurrences "aabcdefg" dict 1
             assertAreEqual 2 result.["a"];
 
-        "countOcccurence will add a new entry for a new 2 character string occurence and init to 1",
+        "countOcccurrence will add a new entry for a new 2 character string occurrence and init to 1",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 2
+            let result = countOccurrences "abcdefg" dict 2
             assertAreEqual 1 result.["ab"];
 
-        "countOcccurence will return 2 occurences if a 2 character string occures twice",
+        "countOcccurrence will return 2 occurrences if a 2 character string occures twice",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdeabfg" dict 2
+            let result = countOccurrences "abcdeabfg" dict 2
             assertAreEqual 2 result.["ab"];
 
-        "countOcccurence will return 0 for a string with 2 characters that is not in the input",
+        "countOcccurrence will return 0 for a string with 2 characters that is not in the input",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 2
+            let result = countOccurrences "abcdefg" dict 2
             assertIsFalse (result.ContainsKey "ba");    
 
-        "countOcccurence will add a new entry for a new 3 character string occurence and init to 1",
+        "countOcccurrence will add a new entry for a new 3 character string occurrence and init to 1",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 3
+            let result = countOccurrences "abcdefg" dict 3
             assertAreEqual 1 result.["abc"];
 
-        "countOcccurence will return 2 occurences if a 3 character string occures twice",
+        "countOcccurrence will return 2 occurrences if a 3 character string occures twice",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdeabcfg" dict 3
+            let result = countOccurrences "abcdeabcfg" dict 3
             assertAreEqual 2 result.["abc"];
 
-        "countOcccurence will return 0 for a string with 3 characters that is not in the input",
+        "countOcccurrence will return 0 for a string with 3 characters that is not in the input",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 3
+            let result = countOccurrences "abcdefg" dict 3
             assertIsFalse (result.ContainsKey "bca");
 
-        "countOcccurence will add a new entry for a new 4 character string occurence and init to 1",
+        "countOcccurrence will add a new entry for a new 4 character string occurrence and init to 1",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 4
+            let result = countOccurrences "abcdefg" dict 4
             assertAreEqual 1 result.["abcd"];
 
-        "countOcccurence will return 2 occurences if a 4 character string occures twice",
+        "countOcccurrence will return 2 occurrences if a 4 character string occures twice",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdeabcdfg" dict 4
+            let result = countOccurrences "abcdeabcdfg" dict 4
             assertAreEqual 2 result.["abcd"];
 
-        "countOcccurence will return 0 for a string with 4 characters that is not in the input",
+        "countOcccurrence will return 0 for a string with 4 characters that is not in the input",
         fun() ->
             let dict = new Dictionary<string, int>()
-            let result = countOccurences "abcdefg" dict 4
+            let result = countOccurrences "abcdefg" dict 4
             assertIsFalse (result.ContainsKey "bcaa");
     ]        

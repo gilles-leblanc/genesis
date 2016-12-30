d0686ab7125eb1fb1dfc194955737f6d9fc02b59
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-12-30 14:07:51 -0500

    Modify buildFrequency to use floats and round them.

--- genesis/genesis/name_gen/NameGenerator.fs
+++ genesis/genesis/name_gen/NameGenerator.fs
@@ -8,7 +8,7 @@ let readInputFile (filePath:string) =
     System.IO.File.ReadAllText(filePath)
 
 // Parses a string and count the total number of occurrences of substrings of size length
-let rec countOccurrences (input:string) (occurrenceTable:Map<string, int>) length = 
+let rec countOccurrences (input:string) (occurrenceTable:Map<string, float>) length = 
     let adjLen = length - 1
     
     match input |> Seq.toList with
@@ -18,19 +18,21 @@ let rec countOccurrences (input:string) (occurrenceTable:Map<string, int>) lengt
 
         // add current occurrence to the occurrence table
         let updatedMap = match occurrenceTable.ContainsKey (occurrence) with
-                         | true -> occurrenceTable.Add(occurrence, occurrenceTable.[occurrence] + 1)
-                         | false -> occurrenceTable.Add(occurrence, 1)
+                         | true -> occurrenceTable.Add(occurrence, occurrenceTable.[occurrence] + 1.0)
+                         | false -> occurrenceTable.Add(occurrence, 1.0)
 
         // call the function recursively with the rest of the string
         countOccurrences (tail |> Array.ofList |> String) updatedMap length
     | _ -> occurrenceTable
 
-// build frequency table    
-    // map again with actual / total
-let buildFrenquencyTable (occurrenceTable:Map<string, int>) =
+// Given a map that contains the a collection of string and their respective counts, return a
+// frequency map that is obtained by count / total count
+let buildFrenquencyTable (occurrenceTable:Map<string, float>) =
     // fold occurence dict, count total
-    let total = Map.fold (fun acc key value -> acc + value) 0 occurrenceTable
-    total
+    let total = Map.fold (fun acc key value -> acc + value) 0.0 occurrenceTable
+    
+    // map over previous map and replace values with count / total count
+    Map.map (fun key value -> Math.Round(value / total, 6)) occurrenceTable
 
 
 // Given an input file create a probability table for the different letters in the file
@@ -38,4 +40,5 @@ let buildProbabilityTable (filePath:string) length =
     let input = readInputFile filePath
     let initialDictionary = Map.empty
 
-    countOccurrences input initialDictionary length
+    countOccurrences input initialDictionary length 
+    |> buildFrenquencyTable
--- genesis_tests/genesis_tests/NameGenTests.fs
+++ genesis_tests/genesis_tests/NameGenTests.fs
@@ -12,73 +12,101 @@ let nameGenTests =
     [
         "countOcccurrence will add a new entry for a new string occurrence and init to 1",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 1
-            assertAreEqual 1 result.["a"];
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 1
+            assertAreEqual 1.0 result.["a"];
 
         "countOcccurrence will return 0 for a string that is not in the input",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 1
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 1
             assertIsFalse (result.ContainsKey "z");    
 
         "countOcccurrence will return 2 occurrences if a string occures twice",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "aabcdefg" dict 1
-            assertAreEqual 2 result.["a"];
+            let map = Map.empty
+            let result = countOccurrences "aabcdefg" map 1
+            assertAreEqual 2.0 result.["a"];
 
         "countOcccurrence will add a new entry for a new 2 character string occurrence and init to 1",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 2
-            assertAreEqual 1 result.["ab"];
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 2
+            assertAreEqual 1.0 result.["ab"];
 
         "countOcccurrence will return 2 occurrences if a 2 character string occures twice",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdeabfg" dict 2
-            assertAreEqual 2 result.["ab"];
+            let map = Map.empty
+            let result = countOccurrences "abcdeabfg" map 2
+            assertAreEqual 2.0 result.["ab"];
 
         "countOcccurrence will return 0 for a string with 2 characters that is not in the input",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 2
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 2
             assertIsFalse (result.ContainsKey "ba");    
 
         "countOcccurrence will add a new entry for a new 3 character string occurrence and init to 1",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 3
-            assertAreEqual 1 result.["abc"];
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 3
+            assertAreEqual 1.0 result.["abc"];
 
         "countOcccurrence will return 2 occurrences if a 3 character string occures twice",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdeabcfg" dict 3
-            assertAreEqual 2 result.["abc"];
+            let map = Map.empty
+            let result = countOccurrences "abcdeabcfg" map 3
+            assertAreEqual 2.0 result.["abc"];
 
         "countOcccurrence will return 0 for a string with 3 characters that is not in the input",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 3
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 3
             assertIsFalse (result.ContainsKey "bca");
 
         "countOcccurrence will add a new entry for a new 4 character string occurrence and init to 1",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 4
-            assertAreEqual 1 result.["abcd"];
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 4
+            assertAreEqual 1.0 result.["abcd"];
 
         "countOcccurrence will return 2 occurrences if a 4 character string occures twice",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdeabcdfg" dict 4
-            assertAreEqual 2 result.["abcd"];
+            let map = Map.empty
+            let result = countOccurrences "abcdeabcdfg" map 4
+            assertAreEqual 2.0 result.["abcd"];
 
         "countOcccurrence will return 0 for a string with 4 characters that is not in the input",
         fun() ->
-            let dict = Map.empty
-            let result = countOccurrences "abcdefg" dict 4
+            let map = Map.empty
+            let result = countOccurrences "abcdefg" map 4
             assertIsFalse (result.ContainsKey "bcaa");
+
+        "buildFrenquencyTable will correctly compute values with 1 letter substrings",
+        fun() ->
+            let map = Map.empty
+            let probabilities = countOccurrences " James John Max Gary Jess Gilles Mary " map 1 
+                                |> buildFrenquencyTable
+            assertAreEqual 0.210526 probabilities.[" "]            
+            
+        "buildFrenquencyTable will correctly compute values with 1 letter substrings (2)",
+        fun() ->
+            let map = Map.empty
+            let probabilities = countOccurrences " James John Max Gary Jess Gilles Mary " map 1 
+                                |> buildFrenquencyTable
+            assertAreEqual 0.026316 probabilities.["m"]
+
+        "buildFrenquencyTable will correctly compute values with 1 letter substrings (3)",
+        fun() ->
+            let map = Map.empty
+            let probabilities = countOccurrences " James John Max Gary Jess Gilles Mary " map 1 
+                                |> buildFrenquencyTable
+            assertAreEqual 0.078947 probabilities.["J"]
+
+        "buildFrenquencyTable will correctly compute values with 2 letter substrings",
+        fun() ->
+            let map = Map.empty
+            let probabilities = countOccurrences " James John Max Gary Jess Gilles Mary " map 2
+                                |> buildFrenquencyTable
+            assertAreEqual 0.054054 probabilities.["ar"]       
     ]        
